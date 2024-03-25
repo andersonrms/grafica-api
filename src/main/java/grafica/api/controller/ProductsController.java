7f36152a4b616a5d91d171068b591fe2e8323045
anderson ramos <andersonrms15@gmail.com>
2024-03-25 20:14:45 -0300

    feat: improve products controller using response entity

--- src/main/java/grafica/api/controller/ProductsController.java
+++ src/main/java/grafica/api/controller/ProductsController.java
@@ -10,8 +10,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 
 @RestController
@@ -22,29 +24,44 @@ public class ProductsController {
 
     @PostMapping()
     @Transactional
-    public void create(@Valid @RequestBody ProductDTO productDTO) {
+    public ResponseEntity create(@Valid @RequestBody ProductDTO productDTO, UriComponentsBuilder builder) {
         var product = new Product(productDTO);
         repository.save(product);
+
+        var uri = builder.path("/product/{id}").buildAndExpand(product.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new ProductDTO(product));
     }
 
     @GetMapping()
-    public Page<ProductDTO> list(@PageableDefault(size = 10, sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable){
-        return repository.findAllByActiveTrue(pageable).map(ProductDTO::new);
+    public ResponseEntity<Page<ProductDTO>> list(@PageableDefault(size = 10, sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable){
+        var page = repository.findAllByActiveTrue(pageable).map(ProductDTO::new);
+        return ResponseEntity.ok(page);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity list(@PathVariable Long id){
+        var product = repository.getReferenceById(id);
+
+        return ResponseEntity.ok(new ProductDTO(product));
+    }
+
 
     @PutMapping()
     @Transactional
-    public ProductDTO update(@Valid @RequestBody UpdateProductDTO updateProductDTO){
+    public ResponseEntity<ProductDTO> update(@Valid @RequestBody UpdateProductDTO updateProductDTO){
         var product = repository.getReferenceById(updateProductDTO.id());
         product.update(updateProductDTO);
 
-        return new ProductDTO(product);
+        return ResponseEntity.ok(new ProductDTO(product));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void delete(@PathVariable Long id){
+    public ResponseEntity delete(@PathVariable Long id){
         var product = repository.getReferenceById(id);
         product.delete();
+
+        return ResponseEntity.noContent().build();
     }
 }
